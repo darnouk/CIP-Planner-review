27259fa5b238b1a59700b3157b6b0e97be2b3a01
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-07-01 20:05:02 -0500

    main.js set up

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
 // Add all scripts to the JS folder
+import Map from "@arcgis/core/Map";
+import MapView from "@arcgis/core/views/MapView";
+
+const map = new Map({
+  basemap: "topo-vector"
+});
+
+const view = new MapView({
+  container: "viewDiv",
+  map: map,
+  center: [-118.805, 34.027],
+  zoom: 13
+});
