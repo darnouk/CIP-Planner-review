8ef3f19eddcb598ab434e0caa8928fe1eb9634b9
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-07-04 06:21:02 -0500

    basemap

--- js/main.js
+++ js/main.js
@@ -1,23 +1,13 @@
-// Add all scripts to the JS folder
-var map;
+import Map from "https://js.arcgis.com/4.21/esri/Map.js";
+import MapView from "https://js.arcgis.com/4.21/esri/views/MapView.js";
 
-function createMap(){
-  map = L.map('mapid', {
-      center: [44.5, -85.0],
-      zoom: 6
-  });
-
-  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}', {
-      attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
-      maxZoom: 13
-  }).addTo(map);
-
-  getData(map);
-};
+const map = new Map({
+  basemap: "topo"  // Use the topographic basemap
+});
 
 const view = new MapView({
-  container: "viewDiv",
+  container: "viewDiv",   // Reference to the map container
   map: map,
-  center: [-87.9065, 43.0389],
-  zoom: 12
+  center: [-87.9065, 43.0389],  // Longitude, latitude of Milwaukee, WI
+  zoom: 12  // Zoom level
 });
