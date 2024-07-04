8289696f9222e620438594e5d33e385db205013a
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-07-04 06:40:15 -0500

    debug

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 import esriConfig from "https://js.arcgis.com/4.25/esri/config.js";
 import Map from "https://js.arcgis.com/4.25/esri/Map.js";
 import MapView from "https://js.arcgis.com/4.25/esri/views/MapView.js";
-import Search from "https://js.arcgis.com/4.25/esri/widgets/Search.js";
+//import Search from "https://js.arcgis.com/4.25/esri/widgets/Search.js";
 
 esriConfig.apiKey = "AAPKba612eedccd74bbabe0e040dec19190bkMVVICJGBITVVx-yRWPSKUO0HiwM3rEFvcZUZ52GBZfyakj41U9ixXAI3lslJLyo";
 
@@ -16,7 +16,7 @@ const view = new MapView({
   zoom: 12 // Zoom level
 });
 
-const searchWidget = new Search({
+/* const searchWidget = new Search({
   view: view,
   sources: [{
       url: "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer"
@@ -26,4 +26,4 @@ const searchWidget = new Search({
 view.ui.add(searchWidget, {
   position: "top-left",
   index: 2
-});
+}); */
