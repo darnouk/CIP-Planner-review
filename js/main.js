450aa1a903db8d168a068b5253e31a262a851b0e
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-07-04 07:02:51 -0500

    add search bar

--- js/main.js
+++ js/main.js
@@ -1,23 +1,28 @@
 require([
   "esri/config",
   "esri/Map",
-  "esri/views/MapView"
-], function(esriConfig, Map, MapView) {
+  "esri/views/MapView",
+  "esri/widgets/Search"
+], function(esriConfig, Map, MapView, Search) {
 
   esriConfig.apiKey = "AAPKba612eedccd74bbabe0e040dec19190bkMVVICJGBITVVx-yRWPSKUO0HiwM3rEFvcZUZ52GBZfyakj41U9ixXAI3lslJLyo";
 
   const map = new Map({
-      basemap: "arcgis-imagery"
+      basemap: "arcgis-topographic"
   });
 
 const view = new MapView({
   container: "viewDiv", //reference to the map container
   map: map,
   center: [-87.9065, 43.0389],
-  zoom: 12 // Zoom level
+  zoom: 12
 });
 
-/* const searchWidget = new Search({
+const sources = [{
+  url: "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer"
+}];
+
+const searchWidget = new Search({
   view: view,
   sources: [{
       url: "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer"
@@ -27,6 +32,6 @@ const view = new MapView({
 view.ui.add(searchWidget, {
   position: "top-left",
   index: 2
-}); */
+});
 
 });
