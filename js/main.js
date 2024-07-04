f79078281c6a366272f519f9be79e8ee00c87ff4
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-07-04 06:34:48 -0500

    basemap

--- js/main.js
+++ js/main.js
@@ -1,13 +1,29 @@
-import Map from "https://js.arcgis.com/4.21/esri/Map.js";
-import MapView from "https://js.arcgis.com/4.21/esri/views/MapView.js";
+import esriConfig from "https://js.arcgis.com/4.25/esri/config.js";
+import Map from "https://js.arcgis.com/4.25/esri/Map.js";
+import MapView from "https://js.arcgis.com/4.25/esri/views/MapView.js";
+import Search from "https://js.arcgis.com/4.25/esri/widgets/Search.js";
+
+esriConfig.apiKey = "AAPKba612eedccd74bbabe0e040dec19190bkMVVICJGBITVVx-yRWPSKUO0HiwM3rEFvcZUZ52GBZfyakj41U9ixXAI3lslJLyo";
 
 const map = new Map({
-  basemap: "topo"  // Use the topographic basemap
+    basemap: "topographic"
 });
 
 const view = new MapView({
-  container: "viewDiv",   // Reference to the map container
+  container: "viewDiv", //reference to the map container
   map: map,
-  center: [-87.9065, 43.0389],  // Longitude, latitude of Milwaukee, WI
-  zoom: 12  // Zoom level
+  center: [-87.9065, 43.0389],
+  zoom: 12 // Zoom level
+});
+
+const searchWidget = new Search({
+  view: view,
+  sources: [{
+      url: "https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer"
+  }]
+});
+
+view.ui.add(searchWidget, {
+  position: "top-left",
+  index: 2
 });
