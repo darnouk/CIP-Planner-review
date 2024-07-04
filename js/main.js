693305687b1d283b5519cfaed3a9b5541e48951f
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-07-04 06:12:10 -0500

    basemap

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 // Add all scripts to the JS folder
-import Map from "https://js.arcgis.com/4.21/esri/Map.js";
-import MapView from "https://js.arcgis.com/4.21/esri/views/MapView.js";
+var map;
 
-const map = new Map({
-  basemap: "topo"
-});
+function createMap(){
+  map = L.map('mapid', {
+      center: [44.5, -85.0],
+      zoom: 6
+  });
+
+  L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Ocean/World_Ocean_Base/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Tiles &copy; Esri &mdash; Sources: GEBCO, NOAA, CHS, OSU, UNH, CSUMB, National Geographic, DeLorme, NAVTEQ, and Esri',
+      maxZoom: 13
+  }).addTo(map);
+
+  getData(map);
+};
 
 const view = new MapView({
   container: "viewDiv",
