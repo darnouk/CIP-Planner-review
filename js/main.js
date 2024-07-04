30e8c4c80b2b80bf20bab901ea584dd42806dc8e
dacarlson6 <143907525+dacarlson6@users.noreply.github.com>
2024-07-04 06:03:01 -0500

    debug basemap

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 // Add all scripts to the JS folder
-import Map from "@arcgis/core/Map";
-import MapView from "@arcgis/core/views/MapView";
+import Map from "https://js.arcgis.com/4.21/esri/Map.js";
+import MapView from "https://js.arcgis.com/4.21/esri/views/MapView.js";
 
 const map = new Map({
-  basemap: "topo-vector"
+  basemap: "topo"
 });
 
 const view = new MapView({
   container: "viewDiv",
   map: map,
-  center: [-118.805, 34.027],
-  zoom: 13
+  center: [-87.9065, 43.0389],
+  zoom: 12
 });
